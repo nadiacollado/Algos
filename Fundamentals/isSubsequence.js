0be4549b24ec8ccbf1457f3f747c328e6c2b2067
Nadia Collado <collado.nadia@gmail.com>
2021-02-05 16:56:08 -0500

    isSubsequence: complete

--- Fundamentals/isSubsequence.js
+++ Fundamentals/isSubsequence.js
@@ -3,8 +3,25 @@
 // Time: O(N + M); Space: O(1)
 
 // Input
-  // isSubsequenc('hello', 'hello world') --> true
-  // isSubsequenc('sing', 'sting') --> true
-  // isSubsequenc('abc', 'abracadabra) --> true
+  // isSubsequence('hello', 'hello world') --> true
+  // isSubsequence('sing', 'sting') --> true
+  // isSubsequence('abc', 'abracadabra') --> true
   // isSubsequence('abc', 'acb') --> false
 
+// APPROACH
+// multiple pointers! 
+// compare each element of the first string to the second string only moving up the first string when the current element is found in the second string
+
+// edge case: what if the given string exists within the target string in order but some of the letters from the given string are interpersed out of of order before the correct order appears?
+
+  function isSubsequence(str1, str2) {
+    let i = 0;
+    let j = 0;
+    if (!str1) return true;
+    while (j < str2.length) {
+      if (str2[j] === str1[i]) i++;
+      if (i === str1.length) return true;
+      j++;
+    }
+    return false;
+  }
