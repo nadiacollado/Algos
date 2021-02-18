d0e733d5d4899023f37aa11b9dd537609788f8da
Nadia Collado <collado.nadia@gmail.com>
2021-02-18 16:15:10 -0500

    collectOddValues: complete

---
+++ Recursion/collectOddValues.js
@@ -0,0 +1,36 @@
+// Helper Method recursion
+
+function collectOddValues(arr) {
+  let result = [];
+
+  function helper(helperInput) {
+    if (helperInput.length === 0) {
+      return;
+    }
+  
+    if (helperInput[0] % 2 !== 0) {
+      result.push(helperInput[0]);
+    }
+    helper(helperInput.slice(1))
+  }
+
+  helper(arr)
+  return result;
+}
+
+// Pure recursion
+
+function collectOddValues(arr) {
+  let newArr = [];
+
+  if (arr.length === 0) {
+    return newArr;
+  }
+
+  if (arr[0] % 2 !== 0) {
+    newArr.push(arr[0]);
+  }
+
+  newArr = newArr.concat(collectOddValues(arr.slice(1)));
+  return newArr;
+}
--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -1,6 +1,6 @@
 // An easy example of recursion
 
 function factorial(num) {
-  if (num === 1) return 1
-  return num * (factorial(num - 1))
+  if (num === 1) return 1;
+  return num * (factorial(num - 1));
 }
