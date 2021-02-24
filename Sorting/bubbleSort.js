4792a07dd177a09c57593688a4173776fb89baf1
Nadia Collado <collado.nadia@gmail.com>
2021-02-24 16:34:15 -0500

    bubbleSort: complete

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,35 @@
+// BubbleSort
+
+// O(N^2): average & worst
+// O(N): best
+
+// Slightly more efficient
+function bubble(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+function bubble(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
